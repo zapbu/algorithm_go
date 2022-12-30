5ac60a1744e715cde798afa8128f343137713e93
100tick <117633511+100tick@users.noreply.github.com>
2022-12-30 18:05:40 +0000

    update `bubble_sort.go`

--- sorting/bubble_sort.go
+++ sorting/bubble_sort.go
@@ -4,12 +4,9 @@ import "golang.org/x/exp/constraints"
 
 func BubbleSort[T constraints.Ordered](slice []T) {
 	for i := 0; i < len(slice)-1; i++ {
-		a := slice[i]
 		for j := i + 1; j < len(slice); j++ {
-			b := slice[j]
-			if a > b {
+			if slice[i] > slice[j] {
 				slice[i], slice[j] = slice[j], slice[i]
-				continue
 			}
 		}
 	}
