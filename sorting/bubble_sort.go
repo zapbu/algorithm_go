68b5fac4379eb21dcdf7f24a6b6589a6bef9a7a4
100tick <117633511+100tick@users.noreply.github.com>
2023-01-01 17:14:05 +0900

    update `sorting/bubble_sort.go`

--- sorting/bubble_sort.go
+++ sorting/bubble_sort.go
@@ -8,7 +8,7 @@ func BubbleSort[T constraints.Ordered](arr []T) {
 	}
 
 	for i := 0; i < len(arr)-1; i++ {
-		for j := 0; j < len(arr)-i-1; j++ {
+		for j := 0; j < len(arr)-1-i; j++ {
 			if arr[j] > arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 			}
